deb58c49f5a32230537653c076ee7904ea412762
Ahmerraza12 <ahmer183gamerz@gmail.com>
2025-01-10 16:48:11 +0500

    updates

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -8,7 +8,7 @@ const GetInTouch = () => {
                 <p className=" mt-6 max-w-4xl text-lg md:text-xl leading-8 text-gray-500">
                     We cover all time zones and are available around the clock because our editors are from all around the English-speaking world. We are confident that we can handle any editing requirements you may have because our proofreaders are experts with extensive editing expertise.
                 </p>
-                <a className="mt-4 bg-primary px-4 py-2 text-base font-semibold leading-7 text-black hover:bg-black  border-2 border-dashed  border-black hover:text-primary duration-500 ease-in-out transition focus:outline-none focus:ring border-spacing-5 focus:border-white hover:border-white"
+                <a className="mt-4 bg-primary px-4 py-2 text-base font-semibold leading-7 text-black hover:bg-black  border-2   border-black hover:text-primary duration-500 ease-in-out transition focus:outline-none focus:ring border-spacing-5 focus:border-black hover:border-black"
                     href="/">Get Started Now</a>
             </div>
         </section>
